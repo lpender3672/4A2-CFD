cd310ef6a7b1ccd08d34700eb4e97c5b1dc81c84
lpender3672 <louispender3672@gmail.com>
2024-10-31 17:13:03 +0000

    fixed some definite problems but solver runs even worse :)

--- solver/fortran/stencils.f90
+++ solver/fortran/stencils.f90
@@ -94,7 +94,7 @@
 !     mesh, use the four neighbouring nodes in the plus and minus i and 
 !     j-directions.
 !     INSERT
-      prop_avg(2:ni-1,2:nj-1) = (prop(1:ni-2,1:nj-2) + prop(2:ni-1,1:nj-2) + prop(1:ni-2,2:nj-1) + prop(2:ni-1,2:nj-1)) / 4
+      prop_avg(2:ni-1,2:nj-1) = (prop(1:ni-2,2:nj-1) + prop(3:ni,2:nj-1) + prop(2:ni-1,1:nj-2) + prop(2:ni-1,3:nj)) / 4.0
 
 !     Edge values are also averaged in both the i and j-directions. Parallel to
 !     the boundary the averaging is centred, the averages of two nodes are taken
@@ -102,16 +102,13 @@
 !     algorithm is one-sided, the value at the current point is extrapolated
 !     from the values at two nodes away from the boundary point.
 !     INSERT
-      prop_avg(1,2:nj-1) = (prop(1,1:nj-2) + prop(1,2:nj-1)) / 2
-      prop_avg(ni,2:nj-1) = (prop(ni,1:nj-2) + prop(ni,2:nj-1)) / 2
-      prop_avg(2:ni-1,1) = (prop(1:ni-2,1) + prop(2:ni-1,1)) / 2
-      prop_avg(2:ni-1,nj) = (prop(1:ni-2,nj) + prop(2:ni-1,nj)) / 2
+      prop_avg(1,2:nj-1) = (prop(1,1:nj-2)+prop(1,3:nj)+2*prop(2,2:nj-1)-prop(3,2:nj-1)) / 3.0
+      prop_avg(ni,2:nj-1) = (prop(ni,1:nj-2)+prop(ni,3:nj)+2*prop(ni-1,2:nj-1)-prop(ni-2,2:nj-1)) / 3.0
+      prop_avg(2:ni-1,1) = (prop(1:ni-2,1)+prop(3:ni,1)+2*prop(2:ni-1,2)-prop(2:ni-1,3)) / 3.0
+      prop_avg(2:ni-1,nj) = (prop(1:ni-2,nj)+prop(3:ni,nj)+2*prop(2:ni-1,nj-1)-prop(2:ni-1,nj-2)) / 3.0
 
 !     The corner values are not currently smoothed
-      prop_avg(1,1) = prop(1,1)
-      prop_avg(ni,1) = prop(ni,1)
-      prop_avg(1,nj) = prop(1,nj)
-      prop_avg(ni,nj) = prop(ni,nj)
+      prop_avg([1,ni],[1,nj]) = prop([1,ni],[1,nj])
 
 !     Now apply the artificial viscosity by smoothing "prop" towards "prop_avg",
 !     take (1-sfac) * the calculated value of the property + sfac * the average 
