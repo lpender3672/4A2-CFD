9247ccaba9a969c7118d067e8d5410111d89ebd9
lpender3672 <58957722+lpender3672@users.noreply.github.com>
2024-11-30 12:06:35 +0000

    bump mesh generation is suddenly broken on my PC works on my laptop though seems to stem from as early as mesh generation x data is overwritten with y data

--- solver/fortran/generate_mesh.f90
+++ solver/fortran/generate_mesh.f90
@@ -1,4 +1,12 @@
       
+module gen_mesh
+      use types
+      use routines
+
+      implicit none
+
+      contains
+
       subroutine generate_mesh(geom,g)
 
 !     Create cells of the mesh to cover the domain defined by geometry curves,
@@ -9,16 +17,19 @@
       use routines
       implicit none
       type(t_geometry), intent(in) :: geom
-      type(t_grid), intent(inout) :: g
-      real :: si_a(geom%ni_a), si_b(geom%ni_b), si(g%ni)
+      type(t_grid), allocatable, intent(inout) :: g(:)
+      real :: si_a(geom%ni_a), si_b(geom%ni_b)
+      real, allocatable :: si(:), sj(:)
       integer :: ni, nj
 
 !     Declare integers or any extra variables you need here
-      real :: sj(g%nj) ! Non-dimensional curve length in j-direction
       integer :: i, j ! Loop counter
 
 !     Get the size of the mesh and store locally for convenience
-      ni = g%ni; nj = g%nj;
+      ni = g(1)%ni; nj = g(1)%nj;
+
+      allocate(si(ni))
+      allocate(sj(nj))
 
 !     Calculate the non-dimensional curve lengths of the geometry input and
 !     generate linearly spaced points in i-direction at desired mesh resolution
@@ -29,10 +40,14 @@
 !     Interpolate the geometry curves to the required resolution in the 
 !     i-direction, this allows the mesh to be refined without altering the 
 !     geometry definition file, the data is stored at "j = 1" and "j = nj"
-      call interp(si_a,geom%x_a,si,g%x(:,1))
-      call interp(si_a,geom%y_a,si,g%y(:,1))
-      call interp(si_b,geom%x_b,si,g%x(:,nj))
-      call interp(si_b,geom%y_b,si,g%y(:,nj))
+      call interp(si_a,geom%x_a,si,g(1)%x(:,1))
+      call interp(si_a,geom%y_a,si,g(1)%y(:,1))
+      call interp(si_b,geom%x_b,si,g(1)%x(:,nj))
+      call interp(si_b,geom%y_b,si,g(1)%y(:,nj))
+
+      do i = 1, g(1)%nj
+            write(6,*) 'x(ni,1) = ', g(1)%x(g(1)%ni, i)
+      end do
 
 !     Calculate the coordinates of all the intermediate points within the mesh.
 !     Create a new vector of non-dimensional spacings in the j-direction using 
@@ -44,8 +59,8 @@
             ! g%x(i, 2:-1) = (1 - sj(2:-1)) * g%x(i, 1) + sj(2:-1) * g%x(i, nj)
             ! g%y(i, 2:-1) = (1 - sj(2:-1)) * g%y(i, 1) + sj(2:-1) * g%y(i, nj)
             ! but this does ??? make it make sense
-            g%x(i, 2:nj-1) = (1 - sj(2:nj-1)) * g%x(i, 1) + sj(2:nj-1) * g%x(i, nj)
-            g%y(i, 2:nj-1) = (1 - sj(2:nj-1)) * g%y(i, 1) + sj(2:nj-1) * g%y(i, nj)
+            g(1)%x(i, 2:nj-1) = (1 - sj(2:nj-1)) * g(1)%x(i, 1) + sj(2:nj-1) * g(1)%x(i, nj)
+            g(1)%y(i, 2:nj-1) = (1 - sj(2:nj-1)) * g(1)%y(i, 1) + sj(2:nj-1) * g(1)%y(i, nj)
 
             !do j = 2, nj-1
             !      g%x(i, j) = (1 - sj(j)) * g%x(i, 1) + sj(j) * g%x(i, nj)
@@ -62,8 +77,8 @@
 !     and communicate the position of the walls to the solver in a more 
 !     flexible way. The "wall" variable is an "ni * nj" logical array where 
 !     "true" indicates the node is on a wall.
-      g%wall = .false.
-      g%wall(:,[1,g%nj]) = .true.
+      g(1)%wall = .false.
+      g(1)%wall(:,[1,g%nj]) = .true.
 
 !     Print that the mesh has been created
       write(6,*) 'Interpolated mesh from the bounding geometry curves'
@@ -72,3 +87,4 @@
       end subroutine generate_mesh
 
 
+end module gen_mesh
